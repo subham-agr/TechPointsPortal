5fb632c03dda70db584e85aac69325e82b2f0a45
subham-agr <agrsubham423@gmail.com>
2022-10-25 04:46:39 +0530

    Order Admin Page

---
+++ Frontend/src/Components/Order_admin/order_admin.js
@@ -0,0 +1,107 @@
+import React, { useEffect, useState } from 'react'
+import { styled } from '@mui/material/styles';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import './order_admin.css'
+import axios from "axios";
+import { Button } from '@mui/material';
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  '&:nth-of-type(odd)': {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  '&:last-child td, &:last-child th': {
+    border: 0,
+  },
+}));
+
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
+
+export default function Order_Admin() {
+
+  const [order_adminlist, setorder_admin] = React.useState([]);
+
+  useEffect(() =>{
+  axios.get('http://localhost:8000/order_admin', {headers: {"Content-Type": "application/json"}})
+  .then((res) => {
+    setorder_admin(res.data)
+    console.log(res.data)
+  });
+  }, [])
+
+  function handleClick(event) {
+    // console.log(event.target.id)
+    const data = {
+      order_id:JSON.parse(event.target.id)
+    };
+    axios.post('http://localhost:8000/order_admin',data, {headers: {"Content-Type": "application/json"}})
+  .then((res) => {
+    // setorder_admin(res.data)
+    console.log(res)
+  });
+
+  window.location.reload(true);
+  }
+
+  return (
+    <TableContainer className='table' component={Paper}>
+      <Table sx={{ minWidth: 700 }} aria-label="customized table">
+        <TableHead>
+          <TableRow>
+            <StyledTableCell>Order Id</StyledTableCell>
+            <StyledTableCell >Points</StyledTableCell>
+            <StyledTableCell >Product Id</StyledTableCell>
+            <StyledTableCell >Product Name</StyledTableCell>
+            <StyledTableCell >Status</StyledTableCell>
+            <StyledTableCell >
+              Change Status
+            </StyledTableCell>
+            {/* <StyledTableCell >Protein&nbsp;(g)</StyledTableCell> */}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {order_adminlist.map((row) => (
+            <StyledTableRow key={row.order_id}>
+              <StyledTableCell component="th" scope="row">
+                {row.order_id}
+              </StyledTableCell>
+              <StyledTableCell >{row.points}</StyledTableCell>
+              <StyledTableCell >{row.product_id}</StyledTableCell>
+              <StyledTableCell >{row.product_name}</StyledTableCell>
+              <StyledTableCell >{row.status}</StyledTableCell>
+              <StyledTableCell >
+                <Button id={row.order_id} onClick={handleClick}>Change Status</Button>
+              </StyledTableCell>
+            </StyledTableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
--- Frontend/src/Components/Prizes/prizes.js
+++ Frontend/src/Components/Prizes/prizes.js
@@ -1,21 +1,29 @@
-import React, { useEffect, useState } from 'react'
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import Avatar from '../../static/images/avatar/1.jpeg';
-import product_card from './productdata'
-import Grid from '@mui/material/Grid';
-import Pagination from '@mui/material/Pagination';
-import './prizes.css'
+import React, { useEffect, useRef, useState } from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Avatar from "../../static/images/avatar/1.jpeg";
+import product_card from "./productdata";
+import Grid from "@mui/material/Grid";
+import Pagination from "@mui/material/Pagination";
+import "./prizes.css";
 import axios from "axios";
+// import {Button} from '@mui/material'
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
+} from "@mui/material";
 // import Grid from '@mui/material/Unstable_Grid2';
 
 export default function Prize() {
-
-  const [page,setpage] = React.useState();
+  const [page, setpage] = React.useState();
+  const [open, setopen] = useState(false);
   // var page1 = 12*page;
   // function importAll(r) {
   //   let images = {};
@@ -26,23 +34,29 @@ export default function Prize() {
   // const images = importAll(require.context('../../static/images/logo', false, '/\.jpg/'));
 
   const [productslist, setlist] = React.useState([]);
-  var pages = productslist.length/12+1;
+  // const [idproduct, setproduct] = useState();
+  // var idproduct = useRef();
+  var pages = productslist.length / 12 + 1;
 
-  useEffect(() =>{
+  useEffect(() => {
     axios
-  .get('http://127.0.0.1:8000/products', {headers: {"Content-Type": "application/json"}})
-  .then((res) => {
-  // console.log(res.data)
-  setlist(res.data)
-  // console.log(res.data[0].product_picture)
-  });
-  }, [])
+      .get("http://127.0.0.1:8000/products", {
+        headers: { "Content-Type": "application/json" },
+      })
+      .then((res) => {
+        // console.log(res.data)
+        setlist(res.data);
+        // console.log(res.data[0].product_id)
+      });
+  }, []);
 
   function handleClick(event) {
     const data = {
-      roll_number:JSON.parse(localStorage.getItem('data')).data.roll_number,
-      product_id:JSON.parse(JSON.stringify(event.target.id))
+      roll_number: JSON.parse(localStorage.getItem("data")).data.roll_number,
+      product_id: JSON.parse(JSON.stringify(event.target.id)),
     };
+
+    // console.log(event);
 
       axios
     .post('http://127.0.0.1:8000/products',data, {headers: {"Content-Type": "application/json"}})
@@ -51,44 +65,64 @@ export default function Prize() {
     // setlist(res.data)
     // console.log(res.data[0].product_picture)
     });
-  } 
-
-  const products = productslist.slice(page-12,page).map((item) =>
-  <Grid item xl={4} xs={12} sm={6} md={4}>
-  <Card sx={{ maxWidth: 345 }} key={item.product_id}>
-  <CardMedia
-    component="img"
-    height="140"
-    image={item.product_picture}
-    alt="Product1"
-  />
-  <CardContent>
-    <Typography gutterBottom variant="h5" component="div">
-      {item.product_name}
-    </Typography>
-    <Typography gutterBottom variant="body2" color="text.secondary">
-      {item.product_desc}
-    </Typography>
-    <Typography variant="h5" component="h2">
-      {item.points}
-    </Typography>
-  </CardContent>
-  <CardActions>
-    <Button size="small" id={item.product_id} onClick={handleClick}>Reedem Now</Button>
-    {/* <Button size="small">Add to Cart</Button> */}
-  </CardActions>
-  </Card>
-  </Grid>
-  );
+    // setopen(false);
+  }
 
   const handleChange = (e, p) => {
-    setpage(12*p)
-  }
+    setpage(12 * p);
+  };
 
   return (
     <div className="products">
       <Grid container spacing={2}>
-          {products}
+        {productslist.slice(page - 12, page).map((item) => (
+          <Grid item xl={4} xs={12} sm={6} md={4}>
+            <Card sx={{ maxWidth: 345 }} key={item.product_id}>
+              <CardMedia
+                component="img"
+                height="140"
+                image={item.product_picture}
+                alt="Product1"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  {item.product_name}
+                </Typography>
+                <Typography gutterBottom variant="body2" color="text.secondary">
+                  {item.product_desc}
+                </Typography>
+                <Typography variant="h5" component="h2">
+                  {item.points}
+                </Typography>
+              </CardContent>
+              <CardActions>
+                {/* <Button onClick={() => setopen(true)}>Reedem Now</Button> */}
+                <Button size="small" id={item.product_id} onClick={handleClick}>
+                  Reedem Now
+                </Button>
+                {/* <Dialog open={open} onClose={() => setopen(false)}>
+                  <DialogTitle>Submit the Request?</DialogTitle>
+                  <DialogContent>
+                    <DialogContentText>
+                      Are you sure you want to reedem this product?
+                    </DialogContentText>
+                  </DialogContent>
+                  <DialogActions>
+                    <Button onClick={() => setopen(false)}>Cancel</Button>
+                    <Button
+                      id={idproduct}
+                      autoFocus
+                      onClick={handleClick}
+                    >
+                      Submit
+                    </Button>
+                  </DialogActions>
+                </Dialog> */}
+                {/* <Button size="small">Add to Cart</Button> */}
+              </CardActions>
+            </Card>
+          </Grid>
+))}
       </Grid>
       <div className="pagination">
         <Pagination count={pages} color="primary" onChange={handleChange} />
